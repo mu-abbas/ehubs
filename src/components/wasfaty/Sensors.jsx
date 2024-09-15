02ffc30b2217d551d932b5d1592cd5f66e19a16b
Muhammad Abbas <md.devjs@gmail.com>
2024-09-15 16:11:47 +0300

    fix nav hubs bug

--- src/components/wasfaty/Sensors.jsx
+++ src/components/wasfaty/Sensors.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { LuPenSquare } from "react-icons/lu";
+import { LuPenSquare } from 'react-icons/lu';
 function Sensors() {
   return (
     <section className="py-6 grid grid-cols-4 grid-rows-[min-content_1fr] gap-4 h-[calc(100vh-139px)]">
@@ -7,15 +7,15 @@ function Sensors() {
         <div className="flex items-center gap-2">
           <p className="font-medium">Id:</p>
           <p className="text-darkGray">2492879586</p>
-          <button className='ml-auto'>
-          <LuPenSquare size={14} />
+          <button className="ml-auto">
+            <LuPenSquare size={14} />
           </button>
         </div>
         <div className="flex items-center gap-2">
           <p className="font-medium">Ref:</p>
           <p className="text-darkGray">i5302395</p>
-          <button className='ml-auto'>
-          <LuPenSquare size={14} />
+          <button className="ml-auto">
+            <LuPenSquare size={14} />
           </button>
         </div>
         <div className="flex items-center gap-2">
